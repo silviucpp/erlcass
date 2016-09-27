45d1f6793dc12365a6885e33b1ac718d9390aeec
Silviu Caragea <silviu.caragea@woowteam.com>
2016-09-27 16:44:46 +0300

    Adjust supervisor config

--- src/erlcass_sup.erl
+++ src/erlcass_sup.erl
@@ -9,13 +9,8 @@ start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-
-    Children =
-        [
-            proccess(erlcass, 40000)
-        ],
-
-    {ok, {{one_for_one, 10, 1}, Children}}.
+    Children = [proccess(erlcass, 40000)],
+    {ok, {{one_for_one, 5, 1}, Children}}.
 
 proccess(Name, WaitForClose) ->
     {Name, {Name, start_link, []}, permanent, WaitForClose, worker, [Name]}.

01994f294ac39f396bb58657481fa6a5321d537c
Silviu Caragea <silviu.caragea@woowteam.com>
2016-10-04 23:22:55 +0300

    Find the proper path to priv dir

--- src/erlcass_nif.erl
+++ src/erlcass_nif.erl
@@ -43,16 +43,16 @@ load_nif() ->
     ok = erlang:load_nif(SoName, 0).
 
 get_nif_library_path() ->
+    filename:join(get_priv_dir(), ?MODULE).
+
+get_priv_dir() ->
     case code:priv_dir(erlcass) of
         {error, bad_name} ->
-            case filelib:is_dir(filename:join(["..", priv])) of
-                true ->
-                    filename:join(["..", priv, ?MODULE]);
-                false ->
-                    filename:join([priv, ?MODULE])
-            end;
-        Dir ->
-            filename:join(Dir, ?MODULE)
+            EbinDir = filename:dirname(code:which(?MODULE)),
+            AppPath = filename:dirname(EbinDir),
+            filename:join(AppPath, "priv");
+        Path ->
+            Path
     end.
 
 not_loaded(Line) ->
